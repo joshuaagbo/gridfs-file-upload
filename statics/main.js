220e6d7b889cdbc6435aecc3d1a6e3f887620cc0
gem joshua <therealgemjosh@gmail.com>
2019-04-03 14:19:38 +0100

    style modified

---
+++ statics/main.js
@@ -0,0 +1,5 @@
+// alert('hello world')
+// const isActive = document.querySelectorAll('#Home_Card');
+// isActive.forEach(active => active.addEventListener('mouseover', e => {
+//   console.log(e.target);
+// }))
