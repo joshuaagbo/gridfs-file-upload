8475c116a205b6b64e541d84d5769c7fbc7c22a5
gem joshua <therealgemjosh@gmail.com>
2019-04-10 16:08:47 +0100

    ready for deployment

--- statics/main.js
+++
@@ -1,5 +0,0 @@
-// alert('hello world')
-// const isActive = document.querySelectorAll('#Home_Card');
-// isActive.forEach(active => active.addEventListener('mouseover', e => {
-//   console.log(e.target);
-// }))
